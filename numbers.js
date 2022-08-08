f13cb29b3a0d25caace52efa16b1d68563b4ebe5
19Konstantin91 <kostya_klpov@mail.ru>
2022-08-08 23:37:04 +0300

    init commit

--- numbers.js
+++ numbers.js
@@ -1,32 +1,32 @@
-import process from 'process';
-import colors from 'colors'
+import process from "process";
+import colors from "colors";
 
-let numCount = 0
+let numCount = 0;
 
-let [num1, num2] = process.argv.slice(2)
+let [num1, num2] = process.argv.slice(2);
 
-const color = [colors.green, colors.yellow, colors.red]
+const color = [colors.green, colors.yellow, colors.red];
 
 if (!num1 || !num2) {
-	console.log(colors.red('Укажите диапазон чисел для поиска простых чисел!'))
+  console.log(colors.red("Укажите диапазон чисел для поиска простых чисел!"));
 } else {
-	if (num1 < 2) {
-		num1 = 2
-	};
-	for (let i = num1; i <= num2; i++) {
-		let prime = true
+  if (num1 < 2) {
+    num1 = 2;
+  }
+  for (let i = num1; i <= num2; i++) {
+    let prime = true;
 
-		for (let j = 2; j < i; j++) {
-			if (i % j === 0) {
-				prime = false
-			};
-		};
-		if (prime) {
-			console.log(color[numCount % 3](i))
-			numCount++
-		};
-	};
-	if (!numCount) {
-		console.log(colors.red('Нет простых чисел в диапазоне'))
-	};
-};
+    for (let j = 2; j < i; j++) {
+      if (i % j === 0) {
+        prime = false;
+      }
+    }
+    if (prime) {
+      console.log(color[numCount % 3](i));
+      numCount++;
+    }
+  }
+  if (!numCount) {
+    console.log(colors.red("Нет простых чисел в диапазоне"));
+  }
+}
